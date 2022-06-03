e889520af4fda0253374207210be9738aa1cdb93
lunelson <lunelson@gmail.com>
2022-06-03 19:17:18 +0200

    configure CLI development

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Unocss from 'unocss/vite';
+import presetMini from '@unocss/preset-mini';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     Unocss({
-      /* unocss options */
+      rules: [['shit', { color: 'green' }]],
+      presets: [presetMini()],
     }),
   ],
 });
